a2b544f21e25d998ce2c53dd2ea588f30055fa85
kratoscarry <43996369+kratoscarry@users.noreply.github.com>
2023-04-20 12:53:02 +0300

    Added log file as parameter version.

--- Powrshell.bat
+++ Powrshell.bat
@@ -1,2 +1,2 @@
-Powershell.exe -File  C:\Users\be_ha\Desktop\Powrshell.ps1 "C:\Users\be_ha\Desktop\folder1" "C:\Users\be_ha\Desktop\folder2" 
+Powershell.exe -File  C:\Users\be_ha\Desktop\FolderSync\Powrshell.ps1 "C:\Users\be_ha\Desktop\folder1" "C:\Users\be_ha\Desktop\folder2" 
 Pause
